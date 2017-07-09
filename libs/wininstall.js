39b0f785191b2c040d6972abbfb4b641609da023
comp500 <comp500@users.noreply.github.com>
2017-07-09 21:45:34 +0100

    run import dialog

--- libs/wininstall.js
+++ libs/wininstall.js
@@ -1,11 +1,28 @@
 const tmp = require("tmp");
+const fs = require("fs");
+const spawn = require('child_process').spawn;
 
 module.exports = {};
 
 module.exports.run = function (cert) {
 	return new Promise(function (resolve, reject) {
-		tmp.file({ postfix: '.crt' }, function (err, path, fd) {
-			fs.writeFileSync(path, cert);
+		tmp.file({ postfix: '.crt', keep: true }, function (err, path, fd, cleanup) {
+			if (err) {console.error(err);}
+			console.log(path);
+			fs.writeFile(fd, cert, function (error) {
+				if (error) {console.error(error);}
+				fs.close(fd, function () {
+					var spawnImport = spawn("rundll32", ["cryptext.dll,CryptExtOpenCER", path]);
+					spawnImport.on("close", function (code, signal) {
+						fs.unlink(path); // Delete file, don't check for failure
+						if (code == 0) { // Won't reject on failure
+							resolve();
+						} else {
+							reject();
+						}
+					});
+				});
+			});
 		});
 	});
 };
