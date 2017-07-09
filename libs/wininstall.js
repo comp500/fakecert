22fccaab994d1de4a05863965ff3b851bf1600a9
comp500 <comp500@users.noreply.github.com>
2017-07-09 21:09:18 +0100

    write file order

--- libs/wininstall.js
+++ libs/wininstall.js
@@ -6,7 +6,7 @@ module.exports.run = function (cert) {
 	return new Promise(function (resolve, reject) {
 		tmp.file({ postfix: '.crt' }, function (err, path, fd) {
 			console.log(path);
-			fs.writeFileSync(cert, path);
+			fs.writeFileSync(path, cert);
 		});
 	});
 };
