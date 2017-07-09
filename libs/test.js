2a77a879db873a273ff4049a10c334e31ba1884d
comp500 <comp500@users.noreply.github.com>
2017-07-09 21:01:32 +0100

    remove unused var

--- libs/test.js
+++ libs/test.js
@@ -1,6 +1,6 @@
 var test = require("./gencert.js");
 
-var cert = test.generateCert("Hooli", ["hooli.xyz", "*.hooli.xyz"], function (status) {
+test.generateCert("Hooli", ["hooli.xyz", "*.hooli.xyz"], function (status) {
 	console.log(status);
 }).then(function (cert) {
 	test.certToPem(cert).then(function (pem) {
